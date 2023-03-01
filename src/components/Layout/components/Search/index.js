753e7519127b2e7a45e35b1c8bdb0fd5268ee6d5
phuongvu0804 <99994868+phuongvu0804@users.noreply.github.com>
2023-03-01 13:36:13 +0200

    Feat: + install axios + create apiServices folder + utils folder

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -4,10 +4,12 @@ import { faCircleXmark, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import HeadlessTippy from '@tippyjs/react/headless';
 import { Wrapper as PopperWrapper } from '@/components/Popper';
 import AccountItem from '@/components/AccountItem';
+
 import classNames from 'classnames/bind';
 import styles from './Search.module.scss';
 import { SearchIcon } from '@/components/Icons';
 import { useDebounce } from '@/hooks';
+import * as searchServices from '@/apiServices/searchServices';
 
 const cx = classNames.bind(styles);
 
@@ -26,16 +28,15 @@ function Search() {
             return;
         }
 
-        setLoading(true);
+        const fetchApi = async () => {
+            setLoading(true);
 
-        fetch(
-            `https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(
-                debounced,
-            )}&type=less`,
-        )
-            .then((res) => res.json())
-            .then((res) => setSearchResult(res.data))
-            .finally(() => setLoading(false));
+            const response = await searchServices.search(debounced);
+
+            setSearchResult(response);
+            setLoading(false);
+        };
+        fetchApi();
     }, [debounced]);
 
     const handleClear = () => {
